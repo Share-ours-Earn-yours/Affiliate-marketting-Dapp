d89b301bf65ca433cf41d3c4ce842d70e4464b9d
Vairamuthu M <mvairamuthu2003@gmail.com>
2024-06-21 11:52:52 +0530

    updated the server.js

---
+++ routes/linkRoutes.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const router = express.Router();
+const Link = require("../models/link");
+const { nanoid } = require("nanoid");
+
+router.post("/addLink", async (req, res) => {
+  const { url } = req.body;
+  const shortLink = nanoid(8);
+
+  const newLink = new Link({ url, shortLink });
+
+  try {
+    await newLink.save();
+    res.status(201).json(newLink);
+  } catch (error) {
+    res.status(500).json({ error: "Error creating the link" });
+  }
+});
+
+router.get("/links", async (req, res) => {
+  try {
+    const links = await Link.find();
+    res.status(200).json(links);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching links" });
+  }
+});
+
+router.get("/links/:linkId", async (req, res) => {
+  const { linkId } = req.params;
+
+  try {
+    const link = await Link.findById(linkId);
+    if (link) {
+      res.status(200).json(link);
+    } else {
+      res.status(404).json({ error: "Link not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching the link" });
+  }
+});
+
+router.post("/createShortLink", async (req, res) => {
+  const { url } = req.body;
+  const shortLink = nanoid(8);
+
+  const newLink = new Link({ url, shortLink });
+
+  try {
+    await newLink.save();
+    res.status(201).json(newLink);
+  } catch (error) {
+    res.status(500).json({ error: "Error creating the short link" });
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const linkRoutes = require("./routes/linkRoutes");
+const cors = require("cors");
+const app = express();
+
+const port = process.env.PORT || 3000;
+require("dotenv").config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });
+
+app.get("/", (req, res) => {
+  res.json({ message: "API is working" });
+});
+
+app.use("/api", linkRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
